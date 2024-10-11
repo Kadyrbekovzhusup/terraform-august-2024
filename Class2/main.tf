9641ff33e4aec59a087031b923ae1bbe7bd659e2
Kadyrbekovzhusup <kadyrbekovzhusup@gmail.com>
2024-10-11 01:41:26 +0000

    modf.

--- Class2/main.tf
+++ Class2/main.tf
@@ -8,26 +8,26 @@ resource "aws_key_pair" "deployer" {
   }
 
 
-#   resource "aws_s3_bucket" "example" {
-#   bucket = "kaizen-zhusup1234"
-#   force_destroy = true
-#   }
+  resource "aws_s3_bucket" "example" {
+  bucket = "kaizen-zhusup1234"
+  force_destroy = true
+  }
 
 
-# resource "time_sleep" "wait" {
-#   depends_on = [aws_s3_bucket.example]
+resource "time_sleep" "wait" {
+  depends_on = [aws_s3_bucket.example]
 
-#   create_duration = "10s"
-# }
+  create_duration = "10s"
+}
 
-# resource "aws_s3_object" "object" {
-#   depends_on = [time_sleep.wait]
-#   bucket = "kaizen-zhusup1234"
-#   key    = "main.tf"
-#   source = "main.tf"
-# }
+resource "aws_s3_object" "object" {
+  depends_on = [time_sleep.wait]
+  bucket = "kaizen-zhusup1234"
+  key    = "main.tf"
+  source = "main.tf"
+}
 
-#  resource "aws_s3_bucket" "example2" {
-#   bucket_prefix = "kaizen-"
-#   force_destroy = true
-#   }
+ resource "aws_s3_bucket" "example2" {
+  bucket_prefix = "kaizen-"
+  force_destroy = true
+  }
